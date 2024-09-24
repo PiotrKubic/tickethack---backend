6e1a12a962a25466890828da781f2c76eefd1d4a
Simon Bucki <bucki.simon@gmail.com>
2024-09-24 14:09:53 +0200

    finished search route

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const moment = "moment";
-const mongoose = require("mongoose");
+const moment = require("moment");
 const Trip = require("../models/trips");
+
+router.post("/", async (req, res) => {
+  const { departure, arrival } = req.body;
+  const date = moment(req.body.date, "DD-MM-YYYY");
+  const trips = await Trip.find({
+    departure,
+    arrival,
+    date: {
+      $gte: date.startOf("day").toDate(),
+      $lt: date.endOf("day").toDate(),
+    },
+  });
+  if (trips.length === 0) {
+    res.json({ result: false, message: "No trips found" });
+  } else {
+    res.json({ result: true, trips });
+  }
+});
 
 module.exports = router;
