2ef84304cfc5bdee172b11e49ddfcea1e37e9a6b
Simon Bucki <bucki.simon@gmail.com>
2024-09-24 16:34:54 +0200

    added returns tobooking route funtions

--- routes/bookings.js
+++ routes/bookings.js
@@ -6,13 +6,14 @@ const Cart = require("../models/carts");
 
 router.get("/", async (req, res) => {
   const bookings = await Booking.find().populate("tripId");
-  res.json({ bookings });
+  return res.json({ bookings });
 });
 
 router.post("/book-all", async (req, res) => {
   const cart = await Cart.find();
   await Booking.insertMany(cart);
   await Cart.deleteMany();
+  return res.json({ result: true });
 });
 
 module.exports = router;
